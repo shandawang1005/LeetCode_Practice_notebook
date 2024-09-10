46e895ea6267b4fb527bd9fd5b9d628fa7c40996
shanda <brucewang147862@gmail.com>
2024-09-10 15:37:00 -0700

    some questions added

--- Dynamic Programming/70. Climbing Stairs.js
+++ Dynamic Programming/70. Climbing Stairs.js
@@ -18,3 +18,31 @@
 // 1. 1 step + 1 step + 1 step
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
+//这个问题其实就是fabanacci sequence 记下面那种方法吧，用object快一些
+
+var climbStairs = function (n) {
+  let prv1 = 1;
+  let prv2 = 1;
+
+  for (let i = 0; i < n; i++) {
+    let tmp = prv1;
+    prv1 = prv1 + prv2;
+    prv2 = tmp;
+  }
+
+  return prv2;
+};
+
+
+
+var obj = { 1: 1, 2: 2 }
+var climbStairs = function (n) {
+    if (!obj[n]) {
+        let result = climbStairs(n - 1) + climbStairs(n - 2);
+        obj[n] = result
+        return result
+    }
+    else {
+        return obj[n]
+    }
+};
